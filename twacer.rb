4532d5c1fc147c6eb717d74953cc80da98f2c0d8
Ben Belland <benbelland@gmail.com>
2013-10-16 20:35:34 -0600

    Everything starts somewhere

---
+++ twacer.rb
@@ -0,0 +1,3 @@
+twacer.rb
+#ruby script that checks your tweets for self references, favourites, etc and makes a little pony move or do something silly.
+#compiles weekly scores and makes an animation out of it
